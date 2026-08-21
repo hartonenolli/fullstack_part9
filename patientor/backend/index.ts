5bc97aef31e1fbe5a3f506e6036a343375453cc4
hartonenolli <hartonenolli@gmail.com>
2026-08-21 15:42:13 +0300

    11. Patientor backend, step3

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnosies.ts';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/diagnoses', diagnosesRouter);
 
 const PORT = 3001;
 
@@ -10,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ patientor/backend/services/diagnosesService.ts
@@ -0,0 +1,7 @@
+import diagnosesData from '../data/diagnoses.ts';
+
+const getDiagnoses = () => {
+  return diagnosesData;
+};
+
+export default { getDiagnoses };
