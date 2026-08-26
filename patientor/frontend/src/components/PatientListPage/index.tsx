53301d9a9f2329cff01bcb8fc6ffba012fc9d797
hartonenolli <hartonenolli@gmail.com>
2026-08-26 15:17:37 +0300

    24. Patientor, step 2

--- patientor/frontend/src/components/PatientListPage/index.tsx
+++ patientor/frontend/src/components/PatientListPage/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Box, Table, Button, TableHead, Typography, TableCell, TableRow, TableBody } from '@mui/material';
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 import { PatientFormValues, Patient } from "../../types";
 import AddPatientModal from "../AddPatientModal";
@@ -15,6 +16,7 @@ interface Props {
 }
 
 const PatientListPage = ({ patients, setPatients } : Props ) => {
+  const navigate = useNavigate();
 
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [error, setError] = useState<string>();
@@ -47,6 +49,10 @@ const PatientListPage = ({ patients, setPatients } : Props ) => {
     }
   };
 
+  const handlePatientClick = (id: string) => {
+    navigate(`/patients/${id}`);
+  };
+
   return (
     <div className="App">
       <Box>
@@ -65,7 +71,7 @@ const PatientListPage = ({ patients, setPatients } : Props ) => {
         </TableHead>
         <TableBody>
           {Object.values(patients).map((patient: Patient) => (
-            <TableRow key={patient.id}>
+            <TableRow key={patient.id} onClick={() => handlePatientClick(patient.id)}>
               <TableCell>{patient.name}</TableCell>
               <TableCell>{patient.gender}</TableCell>
               <TableCell>{patient.occupation}</TableCell>
---
+++ patientor/frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  FemaleOutlined,
+  MaleOutlined,
+  TransgenderOutlined,
+} from "@mui/icons-material";
+import type { Patient } from "../../types";
+import PatientService from "../../services/patients";
+
+import {
+  Box,
+  CircularProgress,
+  Stack,
+  Typography,
+} from "@mui/material";
+
+
+const PatientPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [patient, setPatient] = useState<Patient | null>(null);
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      if (!id) {
+        return;
+      }
+
+      try {
+        const fetchedPatient = await PatientService.getById(id);
+        setPatient(fetchedPatient);
+      } catch (error) {
+        console.error("Error fetching patient:", error);
+      }
+    };
+
+    void fetchPatient();
+  }, [id]);
+  
+  const genderIcon = (gender: string) => {
+    switch (gender) {
+      case "male":
+        return <MaleOutlined />;
+      case "female":
+        return <FemaleOutlined />;
+      default:
+        return <TransgenderOutlined />;
+    }
+  };
+
+  return (
+    <>
+      <Typography variant="h4" component="h2" gutterBottom>
+        Patient Page
+      </Typography>
+
+      {patient ? (
+        <Stack spacing={1.5}>
+          <Typography variant="h5">{patient.name} {genderIcon(patient.gender)}</Typography>
+          <Typography><strong>SSN:</strong> {patient.ssn}</Typography>
+          <Typography><strong>Occupation:</strong> {patient.occupation}</Typography>
+          <Typography><strong>Date of Birth:</strong> {patient.dateOfBirth}</Typography>
+        </Stack>
+      ) : (
+        <Box sx={{ display: "flex", alignItems: "center", gap: 2, mt: 2 }}>
+          <CircularProgress size={20} />
+          <Typography>Loading patient data...</Typography>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default PatientPage;
